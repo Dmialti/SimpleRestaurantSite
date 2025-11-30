d15ad28e661bc7c3fd47367f7a80dfb92ddeb912
Dmialti <Tishdima1@gmail.com>
2025-11-30 19:45:59 +0100

    nestJS: move to Mercurius

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticleModule } from './article/article.module';
 import { DishModule } from './dish/dish.module';
 import { ReservationModule } from './reservation/reservation.module';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 
 @Module({
   imports: [
@@ -15,8 +15,8 @@ import { ReservationModule } from './reservation/reservation.module';
     ArticleModule,
     DishModule,
     ReservationModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),

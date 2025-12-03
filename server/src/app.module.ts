a5c24ff097aab70ff7126e1d53263015c63f29cc
Dmialti <Tishdima1@gmail.com>
2025-12-03 14:44:19 +0100

    redis: CacheModule import and configure at app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,8 @@ import { ArticleModule } from './article/article.module';
 import { DishModule } from './dish/dish.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -17,6 +19,13 @@ import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
       driver: MercuriusDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'redis',
+      port: 6379,
+      ttl: 600,
     }),
   ],
 })

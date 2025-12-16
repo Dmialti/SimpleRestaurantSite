d36ed2f71145ca7080855a3d38d54ee1c6fdcfae
Dmialti <Tishdima1@gmail.com>
2025-12-16 17:31:16 +0100

    Move CORS to Nginx and cleanup

--- server/src/main.ts
+++ server/src/main.ts
@@ -23,11 +23,6 @@ async function bootstrap() {
     },
   });
 
-  app.enableCors({
-    origin: 'http://localhost:5000',
-    credentials: true,
-  });
-
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }

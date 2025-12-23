4a2f25e2410ecd59c9887ffd71ab496bfed83831
Dmialti <Tishdima1@gmail.com>
2025-12-23 21:07:31 +0100

    fix: hero gradient corners bug fixed

--- client/src/shared/components/BottomGradient/BottomGradient.tsx
+++ client/src/shared/components/BottomGradient/BottomGradient.tsx
@@ -1,7 +1,7 @@
 const BottomGradient = () => {
   return (
     <div
-      className={`absolute bottom-0 h-[40%] w-full bg-[linear-gradient(#0000_0%,_#000_100%)] opacity-80`}
+      className={`absolute rounded-b-2xl bottom-0 h-[40%] w-full bg-[linear-gradient(#0000_0%,_#000_100%)] opacity-80`}
     />
   );
 };

eebe84e9c1f425f345dc9c7bd5f0ecb25bed8e97
Dmialti <Tishdima1@gmail.com>
2026-01-12 16:08:20 +0100

    feat: +unlighthouse config in ci/cd

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ||
+  process.env.NEXT_PUBLIC_BASE_URL ||
   "https://simple-restaurant-site.vercel.app";
 
 export default function robots(): MetadataRoute.Robots {

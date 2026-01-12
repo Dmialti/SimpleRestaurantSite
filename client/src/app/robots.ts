f267326694837768068392ec8a1094f81c5f64de
Dmialti <Tishdima1@gmail.com>
2026-01-12 15:59:44 +0100

    fix: changed url for sitemap.xml from docs default to a proper one for it to work

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -1,5 +1,9 @@
 import type { MetadataRoute } from "next";
 
+const BASE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL ||
+  "https://simple-restaurant-site.vercel.app";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
@@ -7,6 +11,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/admin/"],
     },
-    sitemap: "https://acme.com/sitemap.xml",
+    sitemap: `${BASE_URL}/sitemap.xml`,
   };
 }
